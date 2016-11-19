e5ed56334a40e7f7c98e2ccf4959adddea976023
Brent Jackson <jxnblk@gmail.com>
2016-11-19 00:01:28 -0500

    Adjust css render

--- server/css.js
+++ server/css.js
@@ -1,15 +1,23 @@
 
 const flatten = require('../src/flatten')
 
+const toArr = obj => Object.keys(obj).map(key => {
+  const value = obj[key]
+  return { key, value }
+})
+
 const createDef = ({ key, value }) => `--${key}: ${value}`
+const createRule = (prop, prefix = '') => ({ key }) => `.${prefix + key} { ${prop}: var(--${key}) }`
 
 module.exports = (req, res, { colors }) => {
   const obj = flatten(colors)
-  const defs = Object.keys(obj).map(key => {
-    const value = obj[key]
-    return createDef({ key, value })
-  })
+  const arr = toArr(obj)
+  const defs = arr.map(createDef)
 
-  return `:root {\n  ${defs.join(';\n  ')}\n}`
+  const props = `:root {\n  ${defs.join(';\n  ')}\n}`
+  const colorRules = arr.map(createRule('color')).join('\n')
+  const bgRules = arr.map(createRule('background-color', 'bg-')).join('\n')
+
+  return [ props, colorRules, bgRules ].join('\n\n')
 }
 
